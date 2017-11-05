94fac8a12e44a38e15e7d11c44e52570feb64051
Ilona Demidenko <demiluri@gmail.com>
2017-11-05 22:22:10 +0100

    Improve deployment even more

--- fabfile.py
+++ fabfile.py
@@ -17,18 +17,33 @@ def update_code():
         run('git stash')
         run('git pull')
 
+def unzip_model():
+    with cd(code_dir):
+        run('unzip -o my_model_trained_x8.h5.zip')
+
 def deploy_code():
     with settings(warn_only=True):
         if check_code_dir().failed:
             git_clone()
 
     update_code()
+    unzip_model()
+
+def update_dependencies():
+    with cd(code_dir):
+        run('./scripts/update-dependencies.sh')
+
+def restart():
+    run('supervisorctl restart fashion-nn-d')
 
 def update_apt():
     run('apt-get update -qqyy')
 
 def install_package(name):
     run('apt-get install -qqyy %s' % name)
+
+def install_nginx():
+    install_package('nginx')
 
 def install_supervisor():
     install_package('supervisor')
@@ -39,25 +54,26 @@ def install_python_3_6():
     install_package('python3.6 python3.6-dev python3.6-venv')
     run('ln -sf /usr/bin/python3.6 /usr/local/bin/python3')
 
-def install_pip_for_python_3_6():
-    run('wget https://bootstrap.pypa.io/get-pip.py')
-    run('python3.6 get-pip.py')
-    run('ln -sf /usr/local/bin/pip /usr/local/bin/pip3')
+def install_unzip():
+    install_package('unzip')
 
 def create_virtualenv():
     with cd(code_dir):
         run('python3 -m venv ./virtualenv3')
 
-def update_dependencies():
+def setup_supervisor_config():
     with cd(code_dir):
-        run('./scripts/update-dependencies.sh')
+        run('cp ./conf/fashion-nn-d.conf /etc/supervisor/conf.d/')
+        run('supervisorctl update')
 
 def prepare():
     update_apt()
+    install_nginx()
     install_supervisor()
     install_python_3_6()
-    install_pip_for_python_3_6()
+    install_unzip()
     deploy_code()
     create_virtualenv()
     update_dependencies()
-    # setup_supervisor_config()
+    setup_supervisor_config()
+    restart()
